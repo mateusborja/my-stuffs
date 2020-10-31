164c11cc70e8067ddae359aa2b9817080e2f02de
mateus.borja <mateus.borja@gmail.com>
2020-10-31 01:01:07 -0400

    iimprimindo array bidimensional

---
+++ src/com/mateusborja/javaarray/ArrayBidimensional.java
@@ -0,0 +1,49 @@
+//java array
+package com.mateusborja.javaarray;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mateus Borja at <www.mateusborja.life>
+ */
+public class ArrayBidimensional {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("qtde de linhas: ");
+        int m = sc.nextInt();
+        System.out.print("qtde de colunas: ");
+        int n = sc.nextInt();
+
+        int[][] mat = new int[m][n];
+
+        System.out.println("digite as linhas e colunas: ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+
+            }
+
+        }
+
+        System.out.println();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mat[i][j] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+        sc.close();
+
+    }
+
+}
