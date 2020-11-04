ea05be1ba62b357bb005dc4fd514134e68166d1b
mateus.borja <mateus.borja@gmail.com>
2020-11-04 00:20:52 -0400

    java abstract class and inheritance

---
+++ src/com/mateusborja/abstractclass/Cogumelo.java
@@ -0,0 +1,15 @@
+//java abstarct class
+package com.mateusborja.abstractclass;
+
+/**
+ *
+ * @author Mateus Borja at <www.mateusborja.life>
+ */
+public class Cogumelo extends Item {
+
+    @Override
+    public void pegar() {
+        System.out.println("Pegando o Cogumelo");
+    }
+
+}
---
+++ src/com/mateusborja/abstractclass/Diamante.java
@@ -0,0 +1,15 @@
+//java abastract class
+package com.mateusborja.abstractclass;
+
+/**
+ *
+ * @author Mateus Borja at <www.mateusborja.life>
+ */
+public class Diamante extends Item {
+
+    @Override
+    public void pegar() {
+        System.out.println("Pegando o Diamante");
+    }
+
+}
