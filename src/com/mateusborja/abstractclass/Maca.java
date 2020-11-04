ebe2b563275f4da26195e23a041f0fec2087404c
mateus.borja <mateus.borja@gmail.com>
2020-11-04 00:21:08 -0400

    abstract class examples

---
+++ src/com/mateusborja/abstractclass/Maca.java
@@ -0,0 +1,15 @@
+//java abstract class
+package com.mateusborja.abstractclass;
+
+/**
+ *
+ * @author Mateus Borja at <www.mateusborja.life>
+ */
+public class Maca extends Item {
+
+    @Override
+    public void pegar() {
+        System.out.println("Pegando a Maça");
+    }
+
+}
---
+++ src/com/mateusborja/abstractclass/Moeda.java
@@ -0,0 +1,15 @@
+//java abstract class
+package com.mateusborja.abstractclass;
+
+/**
+ *
+ * @author Mateus Borja at <www.mateusborja.life>
+ */
+public class Moeda extends Item {
+
+    @Override
+    public void pegar() {
+        System.out.println("Pegando a Moeda");
+    }
+
+}
