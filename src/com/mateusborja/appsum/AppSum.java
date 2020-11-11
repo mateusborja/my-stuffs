d4f4297782e016c7dabe03ea9b13f5709e1dba97
mateus.borja <mateus.borja@gmail.com>
2020-11-11 08:36:09 -0400

    App sum 888 num

--- src/com/mateusborja/appsum/AppSum.java
+++ src/com/mateusborja/appsum/AppSum.java
@@ -1,28 +1,29 @@
 package com.mateusborja.appsum;
+
 /**
  *
  * @author mateus.borja
  */
 public class AppSum {
-    
+
     public static void main(String[] args) {
-        int i,j;
-        
+        int i, j;
+        int cti = 0;
+
         for (i = 100; i < 1000; i++) {
+            cti++;
             for (j = 100; j < 1000; j++) {
-                int n = i+j;
-                if (n==888) {
-                    System.out.println("A soma de " + i + " + " + j + "é igual a = " + n );
+                int n = i + j;
+                if (n == 888) {
+                    System.out.println("A soma de " + i + " + " + j + " igual a = " + n);
                 }
-                
+
             }
-            
+
         }
-               
-        
-        
-        
+
+        System.out.println("\nqtde de numeros: " + cti);
+
     }
-    
-    
+
 }
