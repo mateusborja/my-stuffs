eb6a15f927094748d30f72ab66439aaf3c81c06f
mateus.borja <mateus.borja@gmail.com>
2020-11-13 00:51:32 -0400

    recursividade com calculo de fatorial

--- src/com/mateusborja/javarecursividade/FatorialRecursivo.java
+++ src/com/mateusborja/javarecursividade/FatorialRecursivo.java
@@ -6,7 +6,7 @@
 package com.mateusborja.javarecursividade;
 
 public class FatorialRecursivo {
-    
+
     public static int fatorial(int num) {
 
         if (num == 0) {
@@ -16,20 +16,32 @@ public class FatorialRecursivo {
         return num * fatorial(num - 1);
 
     }
-    
-    static int fatorialR;
-    
+
+    static int fatorialR1;
+    static int fatorialR2;
+    static int fatorialR3;
+    static int fatorialR4;
+
     public static void main(String[] args) {
+
+        fatorialR1 = FatorialRecursivo.fatorial(10);
+        fatorialR2 = FatorialRecursivo.fatorial(5);
+        fatorialR3 = FatorialRecursivo.fatorial(3);
+        fatorialR4 = FatorialRecursivo.fatorial(1);
         
-        fatorialR = FatorialRecursivo.fatorial(10);
-        System.out.println(fatorialR);
-        
+        System.out.println(fatorialR1);
+        System.out.println(fatorialR2);
+        System.out.println(fatorialR3);
+        System.out.println(fatorialR4);
+
         System.out.println("fatorial");
+        int ct = 0;
         for (int i = 0; i < 10; i++) {
-            System.out.println(" "+ FatorialRecursivo.fatorial(i));
-            
+            ct++;
+            System.out.println(ct + " fatorial = " + FatorialRecursivo.fatorial(i));
+
         }
-        
+
     }
 
 }
