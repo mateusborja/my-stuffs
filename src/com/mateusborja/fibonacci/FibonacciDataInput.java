a96bada8520239b5ac8f6031ca053f015876ca6a
mateus.borja <mateus.borja@gmail.com>
2020-11-13 09:46:11 -0400

    fibonacci calc sequence with data input range

---
+++ src/com/mateusborja/fibonacci/FibonacciDataInput.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+package com.mateusborja.fibonacci;
+
+import java.util.Scanner;
+
+public class FibonacciDataInput {
+    
+    public static int fibonacciRecursivo(int num) {
+        
+        if (num < 2) {
+            return 1;
+        }
+        
+        return fibonacciRecursivo(num - 1) + fibonacciRecursivo(num - 2);
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("length fibonacci: ");
+        int fib1 = sc.nextInt();
+        
+        System.out.println("seq fibonacci: ");
+        for (int i = 0; i < fib1; i++) {
+            System.out.print(" " + fibonacciRecursivo(i));
+            
+        }
+        
+        sc.close();
+        
+    }
+    
+}
