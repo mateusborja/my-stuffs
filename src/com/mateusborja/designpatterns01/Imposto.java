521ef63e930a33a8d88fc26c0da1517e536d30ed
Mateus Borja <mateus.borja@gmail.com>
2020-11-16 22:01:51 -0400

    interface imposto recebendo um orçamento e retornando um double

---
+++ src/com/mateusborja/designpatterns01/Imposto.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+package com.mateusborja.designpatterns01;
+
+public interface Imposto {
+
+    double calcula(Orcamento orcamento);
+
+}
