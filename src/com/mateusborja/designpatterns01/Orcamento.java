af88a6027cef47aef256afd981d08f3c397adff6
Mateus Borja <mateus.borja@gmail.com>
2020-11-16 22:01:10 -0400

    design pattern simples de calculo de imposto

---
+++ src/com/mateusborja/designpatterns01/Orcamento.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+package com.mateusborja.designpatterns01;
+
+public class Orcamento {
+
+    private double valor;
+
+    public Orcamento(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+    
+    
+
+}
