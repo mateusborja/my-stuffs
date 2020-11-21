9f0b2ec9a1520032d2081898fd76b6365aeb1db7
Mateus Borja <mateus.borja@gmail.com>
2020-11-20 20:34:41 -0400

    recursividade com somatorio

--- src/com/mateusborja/javarecursividade/Somatorio.java
+++ src/com/mateusborja/javarecursividade/Somatorio.java
@@ -10,32 +10,33 @@ import java.util.Scanner;
 public class Somatorio {
 
     public static int somatorioDeUmNumero(int num) {
-
-        if (num == 1) {
-            return 1;
+        if (num == 0) {
+            return 0;
         }
 
         return num + somatorioDeUmNumero(num - 1);
 
     }
-    
-    
+
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
-        
-        
+
         System.out.print("digite um número: ");
         int num = sc.nextInt();
-        
+
         System.out.println();
-        System.out.println("somatório de " + num + " é: " + somatorioDeUmNumero(num));
-        
-        
+        System.out.println("somatório de " + num + " é: " + Somatorio.somatorioDeUmNumero(num));
+
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("somatório de " + i + " é: " + Somatorio.somatorioDeUmNumero(i));
+
+        }
+
         sc.close();
-        
-        
-        
+
     }
 
 }
