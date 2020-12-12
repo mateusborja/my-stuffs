6ae0c388c4edfd427f7c89523698648cbe7b52ff
Mateus Borja <mateus.borja@gmail.com>
2020-12-12 10:08:11 -0400

    constante jogo snake

---
+++ src/com/mateusborja/games/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+
+package com.mateusborja.games;
+
+
+public class Constants {
+    
+    static final int WINDOW_WIDTH = 500;
+    static final int WINDOW_HEIGHT = 500;
+    
+    static final String WINDOW_TITLE = "Game Wear";
+
+}
