89f4f65c993151b19829aed4c1852c21a44671cf
Mateus Borja <mateus.borja@gmail.com>
2020-12-12 10:08:00 -0400

    classe snake jogo

---
+++ src/com/mateusborja/games/Snake.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+
+package com.mateusborja.games;
+
+
+public class Snake {
+    
+    public static void main(String[] args) {
+        
+        new GameWindow();
+        
+    }
+
+}
